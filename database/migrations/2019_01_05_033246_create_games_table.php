e428feecc249e2b016ac9570df2862a70634bc53
Valter Cardoso <31062@ufp.edu.pt>
2019-01-20 01:58:30 +0000

    Database Updated

--- database/migrations/2019_01_05_033246_create_games_table.php
+++ database/migrations/2019_01_05_033246_create_games_table.php
@@ -17,13 +17,13 @@ class CreateGamesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
-            $table->unsignedInteger('value_day');
-            $table->unsignedInteger('quantity');
+            $table->unsignedInteger('value_day')->nullable();
+            $table->unsignedInteger('quantity')->nullable();
             $table->string('youtube_link')->nullable();
             $table->string('store_link')->nullable();
             $table->string('game_link')->nullable();
-            $table->string('icon_path');
-            $table->string('image_path');
+            $table->string('icon_path')->nullable();
+            $table->string('image_path')->nullable();
             $table->unsignedInteger('console_id');
             $table->timestamps();
 
