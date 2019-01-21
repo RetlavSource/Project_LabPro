5b27eb575e1f0b3c32a947271b96481675e055a7
Valter Cardoso <31062@ufp.edu.pt>
2019-01-21 17:59:18 +0000

    'Updated Lock'

--- Helpers/views_BACKUP/exemple_inputs/exemple.blade.php
+++ Helpers/views_BACKUP/exemple_inputs/exemple.blade.php
@@ -61,15 +61,15 @@
 
 {{-- URL --}}
 <div class="form-group row">
-        <label for="88888888888888888" class="col-md-3 col-form-label text-md-right">{{ __('88888888888888888') }}</label>
-    
-        <div class="col-md-8">
-            <input id="88888888888888888" type="text" class="form-control{{ $errors->has('88888888888888888') ? ' is-invalid' : '' }}" name="88888888888888888" value="{{ old('88888888888888888') }}" required>
-    
-            @if ($errors->has('88888888888888888'))
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('88888888888888888') }}</strong>
-                </span>
-            @endif
-        </div>
+    <label for="88888888888888888" class="col-md-3 col-form-label text-md-right">{{ __('88888888888888888') }}</label>
+
+    <div class="col-md-8">
+        <input id="88888888888888888" type="text" class="form-control{{ $errors->has('88888888888888888') ? ' is-invalid' : '' }}" name="88888888888888888" value="{{ old('88888888888888888') }}" required>
+
+        @if ($errors->has('88888888888888888'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('88888888888888888') }}</strong>
+            </span>
+        @endif
     </div>
+</div>
